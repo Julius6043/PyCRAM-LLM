20001e4a2df3dcdcf5cb75126b5e538b1eea9d59
Julius Harnack <73881477+Julius6043@users.noreply.github.com>
2024-09-13 11:47:06 +0200

    final finetuning + test prep

--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,7 @@
 from langgraph_code_assistant import generate_plan
 from langgraph_ReWOO import stream_rewoo
 from ReWOO_codeCheck import stream_rewoo_check
+from vector_store_SB import get_retriever
 import sys
 
 ## test case documentation
@@ -8,7 +9,7 @@ task_test1 = """Stelle die Müsli-Packung auf die Kücheninsel."""
 world_test1 = """
 [kitchen = Object('kitchen', ObjectType.ENVIRONMENT, 'kitchen.urdf'), robot = Object('pr2', ObjectType.ROBOT, 'pr2.urdf'), cereal = Object('cereal', ObjectType.BREAKFAST_CEREAL, 'breakfast_cereal.stl', pose=Pose([1.4, 1, 0.95])))]
 """
-#result = generate_plan(task_test, world_test)
+
 
 ## test case simple
 task_test2 = """Can you set the table for breakfast? I want to eat a bowl of cereals with milk."""
@@ -18,7 +19,6 @@ pose=Pose([2.5, 2, 1.02]), color=[1, 0, 0, 1]), cereal = Object("cereal", Object
 "breakfast_cereal.stl", pose=Pose([2.5, 2.3, 1.05]), color=[0, 1, 0, 1]), spoon = Object("spoon", ObjectType.SPOON, 
 "spoon.stl", pose=Pose([2.4, 2.2, 0.85]), color=[0, 0, 1, 1]), bowl = Object("bowl", ObjectType.BOWL, "bowl.stl", 
 pose=Pose([2.5, 2.2, 1.02]), color=[1, 1, 0, 1]), apartment.attach(spoon, 'cabinet10_drawer_top')]"""
-#result = generate_plan(task_test2, world_test2)
 
 
 ## test case middle
@@ -29,7 +29,6 @@ pose=Pose([2.5, 2, 1.02]), color=[1, 0, 0, 1]), cereal = Object("cereal", Object
 "breakfast_cereal.stl", pose=Pose([2.5, 2.3, 1.05]), color=[0, 1, 0, 1]), spoon = Object("spoon", ObjectType.SPOON, 
 "spoon.stl", pose=Pose([2.4, 2.2, 0.85]), color=[0, 0, 1, 1]), bowl = Object("bowl", ObjectType.BOWL, "bowl.stl", 
 pose=Pose([2.5, 2.2, 1.02]), color=[1, 1, 0, 1]), apartment.attach(spoon, 'cabinet10_drawer_top')]"""
-#result = generate_plan(task_test3, world_test3)
 
 
 ## test case complex
@@ -40,7 +39,6 @@ pose=Pose([2.5, 2, 1.02]), color=[1, 0, 0, 1]), cereal = Object("cereal", Object
 "breakfast_cereal.stl", pose=Pose([2.5, 2.3, 1.05]), color=[0, 1, 0, 1]), spoon = Object("spoon", ObjectType.SPOON, 
 "spoon.stl", pose=Pose([2.4, 2.2, 0.85]), color=[0, 0, 1, 1]), bowl = Object("bowl", ObjectType.BOWL, "bowl.stl", 
 pose=Pose([2.5, 2.2, 1.02]), color=[1, 1, 0, 1]), apartment.attach(spoon, 'cabinet10_drawer_top')]"""
-#result = generate_plan(task_test4, world_test4)
 
 
 ## test case room perception
@@ -51,7 +49,6 @@ pose=Pose([2.5, 2, 1.02]), color=[1, 0, 0, 1]), cereal = Object("cereal", Object
 "breakfast_cereal.stl", pose=Pose([2.5, 2.3, 1.05]), color=[0, 1, 0, 1]), spoon = Object("spoon", ObjectType.SPOON, 
 "spoon.stl", pose=Pose([2.4, 2.2, 0.85]), color=[0, 0, 1, 1]), bowl = Object("bowl", ObjectType.BOWL, "bowl.stl", 
 pose=Pose([2.5, 2.2, 1.02]), color=[1, 1, 0, 1]), apartment.attach(spoon, 'cabinet10_drawer_top')]"""
-#result = generate_plan(task_test5, world_test5)
 
 
 ## test case unknown object
@@ -62,7 +59,6 @@ pose=Pose([2.5, 2, 1.02]), color=[1, 0, 0, 1]), cereal = Object("cereal", Object
 "breakfast_cereal.stl", pose=Pose([2.5, 2.3, 1.05]), color=[0, 1, 0, 1]), spoon = Object("spoon", ObjectType.SPOON, 
 "spoon.stl", pose=Pose([2.4, 2.2, 0.85]), color=[0, 0, 1, 1]), bowl = Object("bowl", ObjectType.BOWL, "bowl.stl", 
 pose=Pose([2.5, 2.2, 1.02]), color=[1, 1, 0, 1]), apartment.attach(spoon, 'cabinet10_drawer_top')]"""
-#result = generate_plan(task_test6, world_test6)
 
 
 ## test case negative
@@ -73,7 +69,12 @@ pose=Pose([2.5, 2, 1.02]), color=[1, 0, 0, 1]), cereal = Object("cereal", Object
 "breakfast_cereal.stl", pose=Pose([2.5, 2.3, 1.05]), color=[0, 1, 0, 1]), spoon = Object("spoon", ObjectType.SPOON, 
 "spoon.stl", pose=Pose([2.4, 2.2, 0.85]), color=[0, 0, 1, 1]), bowl = Object("bowl", ObjectType.BOWL, "bowl.stl", 
 pose=Pose([2.5, 2.2, 1.02]), color=[1, 1, 0, 1]), apartment.attach(spoon, 'cabinet10_drawer_top')]"""
-#result = generate_plan(task_test7, world_test7)
+
+
+# retriever test
+test_code_retriever = get_retriever(1, 3)
+test_docu_retriever = get_retriever(2, 3)
+
 
 def make_test(test_num, run_num=1):
     task = f"task_test{test_num}"
@@ -87,7 +88,6 @@ def make_test(test_num, run_num=1):
 
     result, plan, filled_plan = generate_plan(task, world)
     with open(f"test{test_num}v{run_num}.txt", "w") as file:
-        test_string = f"Plan:\n{plan}\n\nFilled Plan:\n{filled_plan}\n\nResult:\n{result}"
+        test_string = f"Plan:\n{plan}\n\n----\nFilled Plan:\n{filled_plan}\n\n----\nResult:\n{result}"
         file.write(test_string)
     return result
-
